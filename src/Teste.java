54ef2ad5da2cffafaa0ac4799c42ea983a9eda39
Joao Abdalla <joaoabdalla27@gmail.com>
2023-04-30 21:38:58 -0300

    conceito de strategy java

---
+++ src/Teste.java
@@ -0,0 +1,51 @@
+import dio.desafio.gof.SingletonEager;
+import dio.desafio.gof.SingletonLazy;
+import dio.desafio.gof.SingletonLazyHolder;
+import dio.strategy.Comportamento;
+import dio.strategy.ComportamentoAgressivo;
+import dio.strategy.ComportamentoDefensivo;
+import dio.strategy.ComportamentoNormal;
+import dio.strategy.Robo;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        //Singleton
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder= SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder= SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+        //Comportamento
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+
+
+    }
+}
